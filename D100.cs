723a4fe435efd6f78bf03fc07ea7c5312866c1eb
Sam Kennedy <samjameskennedy@gmail.com>
2024-08-08 20:41:32 +0100

    Add settings

---
+++ D100.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace LootableSpells
+{
+    public class D100
+    {
+        public static bool Roll(int threshold)
+        {
+            return UnityEngine.Random.Range(0, 99) < threshold;
+        }
+    }
+}
--- LootableSpellsMod.cs
+++ LootableSpellsMod.cs
@@ -27,6 +27,11 @@ namespace LootableSpells
         public static LootableSpellsMod Instance;
         static Mod mod;
 
+        private bool shopsHaveSpellPages;
+        private bool npcsDropSpellPages;
+        private bool dungeonsHaveSpellPages;
+        private float spellPageFrequency;
+
         [Invoke(StateManager.StateTypes.Start, 0)]
         public static void Init(InitParams initParams)
         {
@@ -40,14 +45,16 @@ namespace LootableSpells
         {
             Instance = this;
 
-            // ModSettings settings = mod.GetSettings();
-            // string spawnFrequencySection = "Spawn Frequency";
-            // spellPageShopFrequency = settings.GetFloat(spawnFrequencySection, "Spell Page Shop Spawn Frequency");
+            ModSettings settings = mod.GetSettings();
+            shopsHaveSpellPages = settings.GetValue<bool>("Availability", "Shops");
+            dungeonsHaveSpellPages = settings.GetValue<bool>("Availability", "DungeonLoot");
+            npcsDropSpellPages = settings.GetValue<bool>("Availability", "NPCLoot");
+            spellPageFrequency = settings.GetValue<float>("Availability", "FrequencyMultiplier");
 
-            InitMod(/*TODO: Mod settings*/);
+            InitMod();
         }
 
-        void InitMod(/*TODO: Mod settings*/)
+        void InitMod()
         {
             Debug.Log("Begin mod init: Lootable Spells");
 
@@ -57,10 +64,14 @@ namespace LootableSpells
             GameObject go = new GameObject("LootableSpells_SpellbookPageBehaviour");
             go.AddComponent<SpellbookPageBehaviour>();
 
-            //TODO Make these settings
-            PlayerActivate.OnLootSpawned += AddSpellPages_OnLootSpawned;
-            EnemyDeath.OnEnemyDeath += AddSpellPages_OnEnemyDeath;
-            LootTables.OnLootSpawned += AddSpellPages_OnDungeonLootSpawned;
+            if (shopsHaveSpellPages)
+                PlayerActivate.OnLootSpawned += AddSpellPages_OnLootSpawned;
+
+            if (dungeonsHaveSpellPages)
+                LootTables.OnLootSpawned += AddSpellPages_OnDungeonLootSpawned;
+
+            if (npcsDropSpellPages)
+                EnemyDeath.OnEnemyDeath += AddSpellPages_OnEnemyDeath;
 
             Debug.Log("Finished mod init: Lootable Spells");
             mod.IsReady = true;
@@ -83,7 +94,7 @@ namespace LootableSpells
             book stores have the best on average and most commonly stocked, 
             and general stores being the lowest quality but a moderate chance, etc.
         */
-        public static void AddSpellPages_OnLootSpawned(object sender, ContainerLootSpawnedEventArgs e)
+        public void AddSpellPages_OnLootSpawned(object sender, ContainerLootSpawnedEventArgs e)
         {
             DaggerfallInterior interior = GameManager.Instance.PlayerEnterExit.Interior;
             if (interior == null || e.ContainerType != LootContainerTypes.ShopShelves)
@@ -112,7 +123,7 @@ namespace LootableSpells
                     return;
             }
 
-            if (D100.Roll(spellbookPageChance))
+            if (D100.Roll((int)(spellbookPageChance * spellPageFrequency)))
             {
                 int numSpellPages = UnityEngine.Random.Range(1, maxPagesPerShelf);
 
@@ -128,7 +139,7 @@ namespace LootableSpells
             }
         }
 
-        public static void AddSpellPages_OnEnemyDeath(object sender, EventArgs e)
+        public void AddSpellPages_OnEnemyDeath(object sender, EventArgs e)
         {
             EnemyDeath enemyDeath = sender as EnemyDeath;
             if (enemyDeath == null)
@@ -167,7 +178,7 @@ namespace LootableSpells
                     return;
             }
 
-            if (D100.Roll(spellbookPageChance))
+            if (D100.Roll((int)(spellbookPageChance * spellPageFrequency)))
             {
                 SpellbookPageItem spellPage = SpellbookPageItem.GenerateRandomSpellbookPage();
                 if (spellPage == null)
@@ -177,7 +188,7 @@ namespace LootableSpells
             }
         }
 
-        public static void AddSpellPages_OnDungeonLootSpawned(object sender, TabledLootSpawnedEventArgs lootArgs)
+        public void AddSpellPages_OnDungeonLootSpawned(object sender, TabledLootSpawnedEventArgs lootArgs)
         {
             PlayerEntity playerEntity = GameManager.Instance.PlayerEntity;
             ItemHelper itemHelper = DaggerfallUnity.Instance.ItemHelper;
@@ -189,33 +200,33 @@ namespace LootableSpells
             switch (lootArgs.LocationIndex)
             {
                 case (int)DFRegion.DungeonTypes.OrcStronghold:
-                    if (D100.Roll(5))
+                    if (D100.Roll((int)(5 * spellPageFrequency)))
                         maxSpellPages = 1;
                     break;
 
                 case (int)DFRegion.DungeonTypes.HumanStronghold:
-                    if (D100.Roll(5))
+                    if (D100.Roll((int)(5 * spellPageFrequency)))
                         maxSpellPages = 1;
                     break;
 
                 case (int)DFRegion.DungeonTypes.DesecratedTemple:
-                    if (D100.Roll(15))
+                    if (D100.Roll((int)(15 * spellPageFrequency)))
                         maxSpellPages = 1;
                     break;
 
                 case (int)DFRegion.DungeonTypes.Coven:
                     //Do witches even use spellbooks?
-                    if (D100.Roll(25))
+                    if (D100.Roll((int)(25 * spellPageFrequency)))
                         maxSpellPages = 2;
                     break;
 
                 case (int)DFRegion.DungeonTypes.VampireHaunt:
-                    if (D100.Roll(20))
+                    if (D100.Roll((int)(20 * spellPageFrequency)))
                         maxSpellPages = 1;
                     break;
 
                 case (int)DFRegion.DungeonTypes.Laboratory:
-                    if (D100.Roll(30))
+                    if (D100.Roll((int)(30 * spellPageFrequency)))
                         maxSpellPages = 2;
                     break;
 
